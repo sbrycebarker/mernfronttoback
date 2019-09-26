b4f585fef9e46b46aa55ad7519d2239efc02585d
sbrycebarker <sbrycesti@gmail.com>
2019-09-25 23:57:17 -0600

    auth

--- server.js
+++ server.js
@@ -1,13 +1,20 @@
 const express = require('express');
-const connecDB = require('./config/db');
+const connectDB = require('./config/db');
 
 const app = express();
 
 //  CONNECT To Database
-
 connectDB();
 
-app.get('/', (req, res) => res.send('Well, hello there!'))
+// Init Middleware
+app.use(express.json({extended: false}))
+
+app.get('/', (req, res) => res.send('Well, hello there!'));
+
+app.use('/api/posts', require('./routes/api/posts'));
+app.use('/api/users', require('./routes/api/users'));
+app.use('/api/auth', require('./routes/api/auth'));
+app.use('/api/profile', require('./routes/api/profile'));
 
 const PORT = process.env.PORT || 5000;
 
