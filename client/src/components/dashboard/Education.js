87a3fb329b898bb7faed4c3e55ad11806c424509
sbrycebarker <sbrycesti@gmail.com>
2020-03-05 02:39:03 -0700

    exp

---
+++ client/src/components/dashboard/Education.js
@@ -0,0 +1,48 @@
+import React , { Fragment } from 'react';
+import PropTypes from 'prop-types';
+import Moment from 'react-moment';
+// import moment from 'moment';
+// import { connect } from 'react-redux';
+
+const Education = ({ education }) => {
+    const educated = education.map( edu => (
+        <tr key={edu._id}>
+            <td>{edu.school}</td>
+            <td className="hide-sm">
+                {edu.degree}
+            </td>
+            <td>
+                <Moment format="YYYY/mm/DD">{edu.from}</Moment> -{" "} 
+                { edu.to === null ? ( ' Now') 
+                : (
+                <Moment format="YYYY/mm/DD">{edu.to}</Moment>
+                    )
+                }
+            </td>
+            <td>
+                { <button className="btn btn-danger">Delete</button> }
+            </td>
+        </tr>
+    ))
+    return (
+        <Fragment>
+         <h2 className="my-2">Education</h2> 
+         <table className="table">
+            <thead>
+                <tr>
+                    <th>School</th>
+                    <th className="hide-sm">Degree</th>
+                    <th className="hide-sm">Years</th>
+                </tr>
+            </thead>
+            <tbody>{educated}</tbody>
+         </table>  
+        </Fragment>
+    )
+}
+
+Education.propTypes = {
+    education: PropTypes.array.isRequired,
+}
+
+export default Education
