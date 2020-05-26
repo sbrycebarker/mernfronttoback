ede7648cbd82b4366354d3b023c77b86bad7436b
sbrycebarker <sbrycesti@gmail.com>
2020-05-26 00:59:52 -0600

    post reduce

--- client/src/components/posts/PostItem.js
+++ client/src/components/posts/PostItem.js
@@ -3,60 +3,66 @@ import PropTypes from 'prop-types';
 import { Link } from 'react-router-dom';
 import Moment from 'react-moment';
 import { connect } from 'react-redux';
-import { addLike, removeLike, deletePost } from '../../actions/post';
+// import { addLike, removeLike, deletePost } from '../../actions/post';
 
-const PostItem = ({
-  auth,
-  post: { _id, text, name, avatar, user, likes, comments, date }
-}) => (
-  <div className="post bg-white p-1 my-1">
-  <div>
-    <a href="profile.html">
-      <img
-        className="round-img"
-        src="https://www.gravatar.com/avatar/205e460b479e2e5b48aec07710c08d50?s=200"
-        alt=""
-      />
-      <h4>John Doe</h4>
-    </a>
-  </div>
-  <div>
-    <p className="my-1">
-      Lorem ipsum dolor sit amet consectetur adipisicing elit. Sint
-      possimus corporis sunt necessitatibus! Minus nesciunt soluta
-      suscipit nobis. Amet accusamus distinctio cupiditate blanditiis
-      dolor? Illo perferendis eveniet cum cupiditate aliquam?
-    </p>
-     <p className="post-date">
-        Posted on 04/16/2019
-    </p>
-    <button type="button" className="btn btn-light">
-      <i className="fas fa-thumbs-up"></i>
-      <span>4</span>
+const PostItem = (
+  { 
+    auth, 
+    post: { _id, text, name, user, avatar, likes, comments, date }}
+  ) => {
+  return (
+    <div class="post bg-white p-1 my-1">
+    <div>
+      <a href="profile.html">
+        <img
+          class="round-img"
+          src={avatar}
+          alt=""
+        />
+        <h4>{name}</h4>
+      </a>
+    </div>
+    <div>
+      <p class="my-1">
+        {text}
+      </p>
+       <p class="post-date">
+        Posted on <Moment format="YYYY/MM/DD">{date}</Moment>
+      </p>
+      <button type="button" class="btn btn-light">
+        <i class="fas fa-thumbs-up"></i> {' '}
+                  {
+          likes.length > 0 && (
+          <span>{likes.length}</span>
+          )
+        }
+      </button>
+      <button type="button" class="btn btn-light">
+        <i class="fas fa-thumbs-down"></i>
+      </button>
+      <Link to={`/post${_id}`} class="btn btn-primary">
+          Discussion {' '}
+          {
+          comments.length > 0 && (
+          <span class='comment-count'>{comments.length}</span>
+          )
+        }
+      </Link>
+      {!auth.loading && user === auth.user._id && (
+      <button      
+      type="button"
+      class="btn btn-danger">
+      <i class="fas fa-times"></i>
     </button>
-    <button type="button" className="btn btn-light">
-      <i className="fas fa-thumbs-down"></i>
-    </button>
-    <a href="post.html" className="btn btn-primary">
-      Discussion <span className='comment-count'>2</span>
-    </a>
-    <button      
-    type="button"
-    className="btn btn-danger"
-  >
-    <i className="fas fa-times"></i>
-  </button>
+      )}
+    </div>
   </div>
-</div>
-);
+  )
+}
 
-PostItem.defaultProps = {
-  showActions: true
-};
 
 PostItem.propTypes = {
   post: PropTypes.object.isRequired,
-  auth: PropTypes.object.isRequired
 };
 
 const mapStateToProps = state => ({
--- client/src/reducers/post.js
+++ client/src/reducers/post.js
@@ -1,6 +1,7 @@
 import {
     GET_POSTS,
     POST_ERROR,
+    UPDATE_LIKES
 } from '../actions/types';
 
 const initialState = {
@@ -13,7 +14,7 @@ const initialState = {
 export default function(state = initialState, action) {
     const { type, payload } = action;
 
-    switch (type) {
+    switch ( type ) {
         case GET_POSTS:
             return {
                 ...state,
@@ -24,6 +25,13 @@ export default function(state = initialState, action) {
             return {
                 ...state,
                 error: payload,
+                loading: false
+            }
+        case UPDATE_LIKES:
+            return {
+                ...state,
+                posts: state.posts.map(post => post._id === payload.id ? { ...post, likes: 
+                payload.likes } : post),
                 loading: false
             }
         default:
