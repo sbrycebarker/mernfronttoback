a5933662fee9ac353d3bdeac4b8fd136c7a6dc15
sbrycebarker <sbrycesti@gmail.com>
2020-05-26 00:59:52 -0600

    post reduce

--- client/src/components/posts/Posts.js
+++ client/src/components/posts/Posts.js
@@ -15,7 +15,7 @@ const Posts = ({ getPosts, post: { posts, loading } }) => {
     <Fragment>
       <h1 className="large text-primary">Posts</h1>
       <p className="lead">
-        <i className="fas fa-user" /> Welcome to the community
+        <i className="fas fa-user"></i> Welcome to the community
       </p>
       <div className="posts">
           {posts.map(post => (
