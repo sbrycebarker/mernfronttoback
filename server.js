e74fd779e4357ce69ae47c938b1d6a4e5d8ea94f
Sergio Barker <sergiobarkerdev@gmail.com>
2025-12-25 00:06:58 -0700

    DB fix

--- server.js
+++ server.js
@@ -28,6 +28,6 @@ if (process.env.NODE_ENV === 'production') {
     })
 }
 
-const PORT = process.env.PORT || 5000;
+const PORT = process.env.PORT || 5003;
 
 app.listen(PORT, () => console.log(`server is listening on port ${PORT}`));
